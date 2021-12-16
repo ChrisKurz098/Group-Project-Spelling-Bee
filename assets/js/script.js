65924f980480768c0217911563dc804c9b74e4f1
CKURZ <ckurz098@gmail.com>
2021-12-16 12:52:33 -0800

    Added fetch for example and definition. Added error handeling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//--------------------------------------------------Begin Fetch Code-----------------------------------------//
+//--------------------------------------------------BEGIN FETCH CODE-----------------------------------------//
 function fetchWord() {
     const wordUrl = "https://random-word-api.herokuapp.com/word?number=1&swear=0";
     let word = "none"
@@ -10,12 +10,42 @@ function fetchWord() {
                 //once we get a word, use that word to fecth an example and def
                 fetchExample(word);
             });
-        } else { console.log("Random Word: NO RESPONSE"); }
+        } else { console.log("Random Word: NO RESPONSE ... retying fetch"); fetchWord(); }
     })
         .catch(function (error) {
-            console.log("Catch Error");
+            console.log("UNABLE TO CONNECT",error);
         });
 }
 
-//---------------------------------------------------END FETCH CODE-----------------------------------------//
+function fetchExample(word) {
+    let exampleUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word;
+    let wordData = [];
+    fetch(exampleUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log("data: ", data);
+                //if there is an example, continue. if not repeat
+                if (data[0].meanings[0].definitions[0].example) {
+                    //check if random word is dictionary word
+                    wordData = [
+                        data[0].word,
+                        data[0].meanings[0].definitions[0].example,
+                        data[0].meanings[0].definitions[0].definition
+                    ]
+                    console.log("Word info: ", wordData);
+                    //pass data to main code
+                    //main(wordData);
+                }
+                else {
+                    console.log("Non valid word. Running fetch again")
+                    fetchWord();
+                }
+            })
+        }
+        else { console.log("Word Example: Error Durring Fetch"); console.log("Word May Not Exsist in Database ... Retry Fetch"); fetchWord();  }
+
+    })
+        .catch(function (error) { console.log("UNABLE TO CONNECT", error); })
+}
 fetchWord();
+//---------------------------------------------------END FETCH CODE-----------------------------------------//
