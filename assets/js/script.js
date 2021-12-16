8fd56117a34f8b170dac3ea38f14224341490efa
CKURZ <ckurz098@gmail.com>
2021-12-16 13:06:11 -0800

    changed the wordData to an object so we can refrence the data by what it is as opposed to an array index. (ie wordData.definition)

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,15 @@ function fetchWord() {
         } else { console.log("Random Word: NO RESPONSE ... retying fetch"); fetchWord(); }
     })
         .catch(function (error) {
-            console.log("UNABLE TO CONNECT",error);
+            console.log("UNABLE TO CONNECT", error);
         });
 }
 
 function fetchExample(word) {
     let exampleUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word;
-    let wordData = [];
+    //we will pass wordData to the main code. 
+    //It is an object containing "word", "example" and "definition"
+    let wordData = {};
     fetch(exampleUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
@@ -27,25 +29,30 @@ function fetchExample(word) {
                 //if there is an example, continue. if not repeat
                 if (data[0].meanings[0].definitions[0].example) {
                     //check if random word is dictionary word
-                    wordData = [
-                        data[0].word,
-                        data[0].meanings[0].definitions[0].example,
-                        data[0].meanings[0].definitions[0].definition
-                    ]
+                    wordData = {
+                        word: data[0].word,
+                        example: data[0].meanings[0].definitions[0].example,
+                        definition: data[0].meanings[0].definitions[0].definition
+                    };
                     console.log("Word info: ", wordData);
                     //pass data to main code
+                    //UNCOMMENT CODE BELOW AND UPDATE WITH MAIN FUNCTIONS NAME WHEN READY
                     //main(wordData);
                 }
                 else {
-                    console.log("Non valid word. Running fetch again")
+                    console.log("Non valid word. Running fetch again");
                     fetchWord();
                 }
             })
         }
-        else { console.log("Word Example: Error Durring Fetch"); console.log("Word May Not Exsist in Database ... Retry Fetch"); fetchWord();  }
+        else {
+            console.log("Word Example: Error Durring Fetch");
+            console.log("Word May Not Exsist in Database ... Retry Fetch");
+            fetchWord();
+        }
 
     })
-        .catch(function (error) { console.log("UNABLE TO CONNECT", error); })
+        .catch(function (error) { console.log("UNABLE TO CONNECT", error); });
 }
 fetchWord();
 //---------------------------------------------------END FETCH CODE-----------------------------------------//
