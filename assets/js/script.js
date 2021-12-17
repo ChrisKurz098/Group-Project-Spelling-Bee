9d855f0397c0b6064405393dfe671e58c932c488
CKURZ <ckurz098@gmail.com>
2021-12-16 18:17:22 -0800

    fixed code I removed by accident

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-
+//--------------------------------------------------BEGIN FETCH CODE-----------------------------------------//
+function fetchWord() {
+    const wordUrl = "https://random-word-api.herokuapp.com/word?number=1&swear=0";
+    let word = "none"
+    fetch(wordUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                word = data[0];
+                console.log("word:", word);
+                //once we get a word, use that word to fecth an example and def
+                fetchExample(word);
+            });
+        } else { console.log("Random Word: NO RESPONSE ... retying fetch"); fetchWord(); }
+    })
+        .catch(function (error) {
+            console.log("UNABLE TO CONNECT", error);
+        });
+}
 
 function fetchExample(word) {
     let exampleUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word;
@@ -35,8 +52,7 @@ function fetchExample(word) {
         }
 
     })
-        .catch(function (error) { console.log("!UNABLE TO CONNECT!", error); });
+        .catch(function (error) { console.log("UNABLE TO CONNECT", error); });
 }
 fetchWord();
 //---------------------------------------------------END FETCH CODE-----------------------------------------//
-
